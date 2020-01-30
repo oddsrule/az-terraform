3c7f4b19af23a647515f2ae074919850eb493da1
Jensen <kirk.jensen@cgi.com>
2020-01-29 21:26:25 -0700

    added storage account

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ Getting used to terraform, will be writing code that originated as az cli comman
 Registering provider with the version parameter works - baby steps
 
 Resource group create works - like I said this is learning to crawl
+
+Both "DisplayName" and "Name" are valid for the location - will stick with Name from here on out.
+Hoping to remember to use the tags for cool stuff later on.
+
+Review this link at some point to see if there's value - still want a good assistant for VSCode and terraform
+https://docs.microsoft.com/en-us/azure/terraform/terraform-vscode-extension
+
--- main.tf
+++ main.tf
@@ -5,9 +5,19 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "terraform-rg"
   location = "westus2"
-
   tags = {
     environment = "dev"
     method      = "terraform"
   }
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                      = "terraform-sa"
+  resource_group_name       = "${azurerm_resource_group.rg.name}"
+  location                  = "${azurerm_resource_group.rg.location}"
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+  tags                      = "${azurerm_resource_group.rg.tags}"
 }
