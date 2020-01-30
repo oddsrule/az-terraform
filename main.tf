2ad2f22edbb84a8d908648b5bec4db59af99e24a
Jensen <kirk.jensen@cgi.com>
2020-01-29 21:08:03 -0700

    updated readme with previous success, added resource group

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "azurerm" {
   version         = "1.38.0"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-rg"
+  location = "West US 2"
+
+  tags = {
+    environment = "dev"
+    method      = "terraform"
+  }
+}
