8360819a56bf0462cf0104e2bde3b307f09fd2db
Jensen <kirk.jensen@cgi.com>
2020-01-30 20:51:57 -0700

    fixed nsg name

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = join("-", [azurerm_virtual_network.vnet, "nsg"])
+  name                = join("-", [azurerm_virtual_network.vnet.name, "nsg"])
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = azurerm_resource_group.rg.tags
