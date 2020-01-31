5f656152a0f920650379092ce502ba6c002fcd39
Jensen <kirk.jensen@cgi.com>
2020-01-30 19:21:11 -0700

    commented out vnet

--- main.tf
+++ main.tf
@@ -26,14 +26,14 @@ resource "azurerm_storage_account" "sa" {
   tags                      = azurerm_resource_group.rg.tags
 }
 
-resource "azure_virtual_network" "vnet" {
-  name                      = azurerm_resource_group.rg.name-vnet01
-  location                  = azurerm_resource_group.rg.location
-  resource_group_name       = azurerm_resource_group.rg.name
-  address_space             = ["10.0.0.0/16"]
-
-  subnet {
-    name           = azurerm_resource_group.rg.name-bst-snet
-    address_prefix = "10.0.1.0/24"
-  }
+#resource "azure_virtual_network" "vnet" {
+#  name                      = azurerm_resource_group.rg.name-vnet01
+#  location                  = azurerm_resource_group.rg.location
+#  resource_group_name       = azurerm_resource_group.rg.name
+#  address_space             = ["10.0.0.0/16"]
+#
+#  subnet {
+#    name           = azurerm_resource_group.rg.name-bst-snet
+#    address_prefix = "10.0.1.0/24"
+#  }
 }
