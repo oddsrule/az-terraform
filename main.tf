bf30f1273806fb81e944c015276bdba95508b58d
Jensen <kirk.jensen@cgi.com>
2020-02-04 22:22:40 -0700

    added RG name to new subnets

--- main.tf
+++ main.tf
@@ -107,24 +107,28 @@ resource "azurerm_subnet" "bastion" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = "BastionSubnet"
   address_prefix       = "10.0.1.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "dmz" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = "DMZSubnet"
   address_prefix       = "10.0.2.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "web" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = "WebSubnet"
   address_prefix       = "10.0.3.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "db" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   name                 = "DBSubnet"
   address_prefix       = "10.0.4.0/24"
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_group" "nsg" {
