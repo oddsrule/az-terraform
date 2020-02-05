561ee56a39235cb3ebc1598512bd7688fb942df9
Jensen <kirk.jensen@cgi.com>
2020-02-04 21:13:31 -0700

    cleaned up vm interface

--- main.tf
+++ main.tf
@@ -191,7 +191,7 @@ resource "azurerm_virtual_machine" "bastion" {
   name                             = join("-", [azurerm_resource_group.rg, var.bastion])
   location                         = azurerm_resource_group.rg.location
   resource_group_name              = azurerm_resource_group.rg.name
-  network_interface_ids            = azurerm_network_interface.bastionnic.id
+  network_interface_ids            = [azurerm_network_interface.bastionnic.id]
   vm_size                          = "Standard_B1ls"
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
