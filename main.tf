dc900df655507ba574fcbeca9a2b3f2b0e96f614
Jensen <kirk.jensen@cgi.com>
2020-02-04 22:58:12 -0700

    fix on public ip

--- main.tf
+++ main.tf
@@ -184,7 +184,7 @@ resource "azurerm_network_security_rule" "bastioninternet" {
 }
 
 resource "azurerm_public_ip" "bastionpublicip" {
-  name                = bastionpublicip
+  name                = "bastionpublicip"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
